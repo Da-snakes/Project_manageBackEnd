e46938d995bd369ae3536c3c7b46ca2c481da947
saif zhioua <85803267+sai4w@users.noreply.github.com>
2022-05-20 21:33:15 -0700

    Update settings.py

--- manager/settings.py
+++ manager/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-34@n)+)i(s!no2iq_48xg6&8+j@hdb$76pn%yftpd1jpz4w$kb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['backend-start.herokuapp.com',]
 
 
 # Application definition
